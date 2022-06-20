9638e9265c5f1e7b94d9af0f1681e23457f821ce
sokin <sergunyapw@gmail.com>
2022-06-20 16:56:18 +0300

    task1

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Magento/elogic', __DIR__);
